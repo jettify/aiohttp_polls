62bc4accb2b7114125e62112f3bcc514b209313f
Nickolai Novik <is.infinity@yahoo.com>
2015-12-18 00:36:27 +0200

    implement index page

--- aiohttp_polls/site.py
+++ aiohttp_polls/site.py
@@ -1,7 +1,31 @@
+import asyncio
+import aiohttp_jinja2
 from aiohttp import web
+from . import db
 
 
 class SiteHandler:
 
-    async def index(self, request):
+    def __init__(self, pg):
+        self.postgres = pg
+
+    @aiohttp_jinja2.template('index.html')
+    @asyncio.coroutine
+    def index(self, request):
+        with (yield from self.postgres) as conn:
+            cursor = yield from conn.execute(
+                db.question.select())
+            records = yield from cursor.fetchall()
+        questions = [dict(q) for q in records]
+        return {
+            'questions': questions
+        }
+
+    async def poll(self, request):
+        return web.Response(body=b'Body Response')
+
+    async def results(self, request):
+        return web.Response(body=b'Body Response')
+
+    async def vote(self, request):
         return web.Response(body=b'Body Response')
