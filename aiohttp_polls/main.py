c33c7f82735afe3e2a816ee4729c787ba779fada
Nickolai Novik <is.infinity@yahoo.com>
2015-12-26 22:49:22 +0200

    fix bug with app finalizer

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -38,12 +38,18 @@ async def init(loop):
     app_handler = app.make_handler()
     srv = await loop.create_server(app_handler, '127.0.0.1', 8080)
     print("Server started at http://127.0.0.1:8080")
-    return srv, handler
+    return srv, app_handler
 
 
 loop = asyncio.get_event_loop()
-srv, handler = loop.run_until_complete(init(loop))
+srv, app_handler = loop.run_until_complete(init(loop))
+
 try:
     loop.run_forever()
 except KeyboardInterrupt:
-    loop.run_until_complete(handler.finish_connections())
+    pass
+finally:
+    loop.run_until_complete(app_handler.finish_connections())
+    srv.close()
+    loop.run_until_complete(srv.wait_closed())
+loop.close()
