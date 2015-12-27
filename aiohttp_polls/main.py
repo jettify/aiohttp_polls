32a585909598df69150dcd94f7dba07a58c4f35c
Nickolai Novik <is.infinity@yahoo.com>
2015-12-27 20:40:22 +0200

    move routes into separate file

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -8,22 +8,11 @@ from aiohttp import web
 
 from aiohttp_polls.views import SiteHandler
 from aiohttp_polls.utils import init_postgres, load_config
+from aiohttp_polls.routes import setup_routes
 
 
 PROJ_ROOT = pathlib.Path(__file__).parent.parent
 TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
-
-
-def setup_routes(app, handler):
-    add_route = app.router.add_route
-    add_route('GET', '/', handler.index)
-    add_route('GET', '/poll/{question_id}', handler.poll, name='poll')
-    add_route('GET', '/poll/{question_id}/results',
-              handler.results, name='results')
-    add_route('POST', '/poll/{question_id}/vote', handler.vote, name='vote')
-    app.router.add_static('/static/',
-                          path=str(PROJ_ROOT / 'static'),
-                          name='static')
 
 
 async def init(loop):
@@ -34,7 +23,7 @@ async def init(loop):
     pg = await init_postgres(conf['postgres'], loop)
 
     handler = SiteHandler(pg)
-    setup_routes(app, handler)
+    setup_routes(app, handler, PROJ_ROOT)
     app_handler = app.make_handler()
 
     host, port = conf['host'], conf['port']
